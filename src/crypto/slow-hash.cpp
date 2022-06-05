3c3d4698810ef37557411f7004b33776fc79b855
LIAKOIN <30527348+codercoins@users.noreply.github.com>
2022-06-05 15:18:45 -0400

    Update slow-hash.cpp

--- src/crypto/slow-hash.cpp
+++ src/crypto/slow-hash.cpp
@@ -43,17 +43,16 @@ namespace Crypto {
 #else
     data = mmap(nullptr, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
 #endif
-      //if (munmap(data, MAP_SIZE) != 0) {
-    //  throw bad_alloc();
-    //}
+  if (data == MAP_FAILED) {
+      throw bad_alloc();
     }
     mlock(data, MAP_SIZE);
   }
 
   cn_context::~cn_context() {
-    if (munmap(data, MAP_SIZE) != 0) {
-      throw bad_alloc();
-    }
+     //if (munmap(data, MAP_SIZE) != 0) {
+    //  throw bad_alloc();
+    //}
   }
 
 #endif
