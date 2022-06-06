2c9c0d07e6a2005415415ca84eb0b6d5b4f6c66e
LIAKOIN <30527348+codercoins@users.noreply.github.com>
2022-06-05 21:36:34 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo apt install libboost-all-dev -y && sudo apt install cmake -y && sudo apt install make -y && sudo apt install build-essential -y && sudo cmake CMakeLists.txt && sudo make -w
+sudo apt install libboost-all-dev -y && sudo apt install cmake -y && sudo apt install make -y && sudo apt install build-essential -y && sudo cmake CMakeLists.txt && sudo make -w && cd src
