d8c3d3b3f1e7f7162dd7d02278bd448a3e636aa8
LIAKOIN <30527348+codercoins@users.noreply.github.com>
2022-06-07 00:18:18 -0400

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -110,8 +110,8 @@ const uint64_t MINIMUM_FEE = 100000;
 const size_t CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE = 20000;
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-   "111.11.11.11:17236",
-  "222.22.22.22:17236",
+   "159.203.1.104:17236",
+  "159.203.1.104:17236",
 };
 
 struct CheckpointData {
